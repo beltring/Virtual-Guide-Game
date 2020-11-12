57ca5b206d8912d926754350021dc22477d840fe
Harkouski <nik7656@yandex.ru>
2020-11-12 10:26:53 +0300

    commit 5

---
+++ Assets/Tests/TestSuit.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class TestSuit {
+
+    [Test]
+    public void GameObjectTest()
+    {
+        //Arrange
+        var gameObject = new GameObject();
+ 
+        //Act
+        //Try to rename the GameObject
+        var newGameObjectName = "My game object";
+        gameObject.name = newGameObjectName;
+ 
+        //Assert
+        //The object has a new name
+        Assert.AreEqual(newGameObjectName, gameObject.name);
+    }
+
+    [Test]
+    public void CatchingErrors()
+    {
+
+    GameObject gameObject = new GameObject("test");
+
+    Assert.Throws<MissingComponentException>(
+        () => gameObject.GetComponent<Rigidbody>().velocity = Vector3.one);
+    }
+
+    [Test]
+    public void LogAssertTest()
+    {
+    //Expect a regular log message
+    LogAssert.Expect(LogType.Log, "Log message");
+    //A log message is expected so without the following line
+    //the test would fail
+    Debug.Log("Log message");
+    //An error log is printed
+    Debug.LogError("Error message");
+    //Without expecting an error log, the test would fail
+    LogAssert.Expect(LogType.Error, "Error message");
+    }
+    
+    [Test]
+    [UnityPlatform (RuntimePlatform.WindowsEditor)]
+    public void RuntimePlatformTest()
+    {
+    Assert.AreEqual(Application.platform, RuntimePlatform.WindowsEditor);
+    }
+}
---
+++ Assets/Video.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Video : MonoBehaviour {
+
+    public GameObject monitorObject;
+
+    //void Out(Collider other)
+    //{
+    //    monitorObject.SetActive(false);
+    //}
+    
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Collider>().tag == "Player")
+        {
+            this.monitorObject.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Collider>().tag == "Player")
+        {
+            this.monitorObject.SetActive(false);
+        }
+    }
+}
