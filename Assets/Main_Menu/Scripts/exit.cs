b484625471083621b5fcc11ebb8321bf6d0c41a1
Harkouski <nik7656@yandex.ru>
2020-11-12 10:16:21 +0300

    commit 3

---
+++ Assets/Main_Menu/Scripts/exit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Experimental.UIElements;
+using System;
+
+public class exit : MonoBehaviour {
+
+    public UnityEngine.UI.Button exitButton; 
+
+    void Start()
+    {
+        exitButton.onClick.AddListener(TaskOnClick);
+    }
+
+    private void TaskOnClick()
+    {
+        Application.Quit();
+    }
+
+  
+}
