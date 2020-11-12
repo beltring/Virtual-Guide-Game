fad4f6cd39ff8fa1b8042c4952a444eed6ffdf1a
Harkouski <nik7656@yandex.ru>
2020-11-12 10:24:39 +0300

    commit 4

---
+++ Assets/Scenes/LoadF.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class LoadF : MonoBehaviour
+{
+    public VideoPlayer vPlayer;
+    public bool enter;
+    bool isPlay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enter)
+        {
+            if(Input.GetKeyUp(KeyCode.F))
+            {
+                isPlay = !isPlay;
+            }
+        }
+        if (isPlay)
+            vPlayer.Play();
+        else
+            vPlayer.Pause();
+
+
+    }
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        enter = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        enter = false;
+    }
+}
---
+++ Assets/Scripts/CodingTogetherStudent.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodingTogetherStudent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public GameObject player;
+    private bool logic;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            logic = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            logic = false;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (logic == true)
+        {
+            GUI.TextArea(new Rect((Screen.width / 2 - 250), (Screen.height / 2 - 100), 400, 80), "Эти студенты пишут скрипт вместе. " +
+                "Издалека может показаться, " +
+                "что пока один трудится, другой не делает ничего, но вблизи слышно, " +
+                "как сосед по парте подсказывает, где была ошибка. " +
+                "Эта методика называется 'парным программированием' и имеет широкое распространение за рубежом.");
+
+        }
+
+    }
+
+}
